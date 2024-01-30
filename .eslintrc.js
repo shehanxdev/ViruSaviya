3ce6971379825b4ebb70be3aba978c4cedb9f9af
Shehan Chanuka <shehanxdev@gmail.com>
2024-01-31 04:37:30 +0530

    feat: added ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+  },
 };
